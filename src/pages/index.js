23da049450f589165d00ab0b9246eebc345a105f
Samy Zogeyb <contact@samyzogeyb.com>
2021-09-16 18:32:55 +0400

    fixed error of exporting getstaticpaths with no dynamic paths

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
-import { useRouter } from "next/dist/client/router";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import Box from "../components/Box/Box";
-import Spinner from "../components/Spinner/Spinner";
 import { getGames } from "../features/gamesSlice/gamesSlice";
 import HomeRoute from "../routes/HomeRoute/HomeRoute";
 
@@ -10,29 +7,16 @@ export default function Home(props) {
 	const { data } = props;
 	const { next, results } = data;
 	const dispatch = useDispatch();
-	const router = useRouter();
 
 	useEffect(() => {
 		dispatch(getGames({ next, results }));
 	}, []);
-
-	if (router.isFallback) {
-		return (
-			<Box>
-				<Spinner />
-			</Box>
-		);
-	}
 
 	return (
 		<>
 			<HomeRoute />
 		</>
 	);
-}
-
-export async function getStaticPaths() {
-	return { paths: [], fallback: true };
 }
 
 export async function getStaticProps() {
